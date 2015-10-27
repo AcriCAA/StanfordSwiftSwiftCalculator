cc7cf7221635bbd34a0e2d76c145fabee835fef7
AcriCAA <acri.corey@gmail.com>
2015-10-27 18:09:45 -0400

    30 minutes into Lesson 2.  Just figuring out why the enter() function was throwing errors.  It have "AnyObject" in the function parameters, which needed to be removed because it is not receiving an object and instead getting the title of the button.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,18 +8,86 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+
+    @IBOutlet weak var display: UILabel!
+//            variable name     type
+// ^^^ syntax
+
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+    
+        
+        let digit = sender.currentTitle!
+        print("digit equals \(digit)")
+        if userIsInTheMiddleOfTypingANumber{
+        display.text = display.text! + digit
+        } else {
+        display.text = digit
+        userIsInTheMiddleOfTypingANumber = true
+        
+        }
     }
+    
+    
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    @IBAction func operate(sender: UIButton) {
+        
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        
+        
+        switch operation {
+        
+        case"×": performOperation {$0 * $1}
+           //        case"÷":
+//        case"+":
+//        case"−":
+        default: break
+        
+        }
+        
     }
-
-
+    
+    
+    func performOperation(operation: (Double, Double) ->Double ){
+        if operandStack.count >= 2{
+        displayValue = operandStack.removeLast() * operandStack.removeLast()
+        enter()
+                }
+    
+    }
+    
+    var operandStack = Array<Double>()
+   
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        print("operandStack = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set
+        {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+    
+    
+ 
 }
 
