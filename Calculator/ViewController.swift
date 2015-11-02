3fa6a006dbddd06f3aa99aeb142e54907a80ebdd
AcriCAA <acri.corey@gmail.com>
2015-11-02 15:08:09 -0500

    Lesson 3 done but calculator is crashing on launch.  Must be something with operand stack

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -15,15 +15,15 @@ class ViewController: UIViewController
 
     @IBOutlet weak var display: UILabel!
 //            variable name     type
-// ^ syntax 
 
     var userIsInTheMiddleOfTypingANumber = false
+    
+    var brain = CalculatorBrain()
     
     @IBAction func appendDigit(sender: UIButton) {
     
         
         let digit = sender.currentTitle!
-        
         
         if userIsInTheMiddleOfTypingANumber{
         display.text = display.text! + digit
@@ -38,55 +38,39 @@ class ViewController: UIViewController
     
     
     
-    
     @IBAction func operate(sender: UIButton) {
         
-        let operation = sender.currentTitle!
+        
         if userIsInTheMiddleOfTypingANumber {
             enter()
         }
-        
-        
-        switch operation {
-        
-        case"×": performOperation {$0 * $1}
-        case"÷": performOperation {$0 / $1}
-        case"+": performOperation {$0 + $1}
-        case"−": performOperation {$1 - $0}
-        case"√": performOperationSqrt { sqrt($0) }
-        default: break
-        
-        }
-        
-    }
     
-    
-    func performOperation(operation: (Double, Double) -> Double ){
-        if operandStack.count >= 2{
-        displayValue = operandStack.removeLast() * operandStack.removeLast()
-        enter()
-                }
-    
-    }
-    
-    func performOperationSqrt(operation: Double -> Double){
-        if operandStack.count >= 1{
-            displayValue = operation(operandStack.removeLast())
-            enter()
-        }
-        
-    }
-    
-    var operandStack = Array<Double>()
-   
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation){
+                displayValue = result
+            }
+                
+            else {
+                displayValue = 0
+            }
 
+    }
+        
+    }
     
+    
+
     
     @IBAction func enter() {
         
         userIsInTheMiddleOfTypingANumber = false
-        operandStack.append(displayValue)
-        print("operandStack = \(operandStack)")
+        if let result = brain.pushOperand(displayValue) {
+        displayValue = result
+        } else {
+        
+            displayValue = 0
+            
+        }
         
     }
     
@@ -103,6 +87,5 @@ class ViewController: UIViewController
     }
     
     
- 
 }
 
